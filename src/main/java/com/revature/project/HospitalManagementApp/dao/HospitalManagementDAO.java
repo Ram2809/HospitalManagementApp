835c9e1cd8defdd498537ef40f4879d847d4f0ad
Ramkumar C <kumarramkumar2000@gmail.com>
2021-07-23 22:56:05 +0530

    DB Connection Completed

---
+++ src/main/java/com/revature/project/HospitalManagementApp/dao/HospitalManagementDAO.java
@@ -0,0 +1,5 @@
+package com.revature.project.HospitalManagementApp.dao;
+
+public interface HospitalManagementDAO {
+
+}
---
+++ src/main/java/com/revature/project/HospitalManagementApp/util/DBUtil.java
@@ -0,0 +1,10 @@
+package com.revature.project.HospitalManagementApp.util;
+
+public class DBUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
